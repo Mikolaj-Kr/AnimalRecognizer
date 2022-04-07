00b041cbe15172513b353e09d186b49d259c32a5
Mikolaj-Kr <mikolak25@gmail.com>
2022-04-07 19:32:51 +0200

    basic functionality

--- app/src/main/java/com/thesims/animalsApp/MainActivity.java
+++ app/src/main/java/com/thesims/animalsApp/MainActivity.java
@@ -5,9 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -18,7 +20,11 @@ import com.google.mlkit.vision.label.ImageLabeler;
 import com.google.mlkit.vision.label.ImageLabeling;
 import com.google.mlkit.vision.label.defaults.ImageLabelerOptions;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     private Bitmap bmp;
@@ -27,13 +33,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bmp = BitmapFactory.decodeResource(getResources(), R.drawable.cat);
+        generateNewAnimal();
     }
 
     public void buttonRecognizeOnClick(View v) {
         InputImage image = InputImage.fromBitmap(bmp, 0);
         ImageLabeler labeler = ImageLabeling.getClient(ImageLabelerOptions.DEFAULT_OPTIONS);
-        System.out.println("1");
         labeler.process(image)
                 .addOnSuccessListener(new OnSuccessListener<List<ImageLabel>>() {
                     @Override
@@ -46,14 +51,43 @@ public class MainActivity extends AppCompatActivity {
                                 recognizedLabel = label.getText();
                             }
                         }
-                        System.out.println(recognizedLabel);
+                        setRecognizedText(recognizedLabel);
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
                         Log.d("RECOGNIZER", e.toString());
+                        System.out.println("error : " + e);
                     }
                 });
+    }
+
+    public void buttonNextOnClick(View v){
+        generateNewAnimal();
+    }
+
+    private void generateNewAnimal(){
+        setRecognizedText("");
+        Map<Integer, String> animalsMap = generateAnimalsMap();
+        List<Integer> animalKeys = new ArrayList<>(animalsMap.keySet());
+        Integer randomKey = animalKeys.get(new Random().nextInt(animalKeys.size()));
+        bmp = BitmapFactory.decodeResource(getResources(), randomKey);
+        ImageView animalImageView = findViewById(R.id.imageView);
+        animalImageView.setBackgroundResource(randomKey);
+    }
+
+    private Map<Integer, String> generateAnimalsMap(){
+        Map<Integer, String> animalsMap = new HashMap<>();
+        animalsMap.put(R.drawable.cat, "@/drawable-v24/cat.bmp");
+        animalsMap.put(R.drawable.bird, "@/drawable-v24/bird.bmp");
+        animalsMap.put(R.drawable.dog, "@/drawable-v24/dog.bmp");
+        animalsMap.put(R.drawable.horse, "@/drawable-v24/horse.bmp");
+        return animalsMap;
+    }
+
+    private void setRecognizedText(String newText){
+        TextView recognizedAnimalText = findViewById(R.id.textViewLabel);
+        recognizedAnimalText.setText(newText);
     }
 }
